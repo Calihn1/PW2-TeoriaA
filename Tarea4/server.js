4e6a1bef7946bad4e97c423a0db144ff8a87f540
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-18 16:17:49 -0500

    Implementación del servidor node, se creo una constante para la base sqlite, para parsear a json el envio de datos, que los archivos de interacción dentro de la carpeta public y conección con la data base

---
+++ Tarea4/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const db = new sqlite3.Database('imdb.db');
+
