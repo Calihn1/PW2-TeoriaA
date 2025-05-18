2822f9c732e9c8684e656c4c0080c66d7a1de649
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-18 16:21:36 -0500

    Se busca todas las peliculas las cuales participo el actor, y se selecciona el titulo, el año, su puntaje, etc. Uso del método post y si encontraba coincidencias las devuelve en caso de no ser asi, retorna error

--- Tarea4/server.js
+++ Tarea4/server.js
@@ -10,3 +10,20 @@ app.use(express.static('public'));
 
 const db = new sqlite3.Database('imdb.db');
 
+
+// Buscar por nombre de actor
+app.post('/search/actor', (req, res) => {
+  const actorName = req.body.actor;
+  const query = `
+    SELECT Movie.Title, Movie.Year, Movie.Score, Movie.Votes
+    FROM Movie
+    JOIN Casting ON Movie.MovieID = Casting.MovieID
+    JOIN Actor ON Actor.ActorId = Casting.ActorId
+    WHERE Actor.Name = ?;
+  `;
+  db.all(query, [actorName], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+});
+
