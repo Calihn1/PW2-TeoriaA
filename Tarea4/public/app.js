49e95d02554ce00be007d71ff7291f96d1a7608c
Calihn1 <ehilacondob@unsa.edu.pe>
2025-05-18 16:40:47 -0500

    Lo mismo mandamos el año y recibimos en un json el resultado, lo casteamos a un String y lo mandamos a realizar una tabla

--- Tarea4/public/app.js
+++ Tarea4/public/app.js
@@ -12,6 +12,18 @@ document.addEventListener('DOMContentLoaded', () => {
     if (data.error || data.length === 0) return showError('Actor not found');
     renderNormalTable(data);
   });
-});
 
+  document.getElementById('searchYearBtn').addEventListener('click', async () => {
+    const year = document.getElementById('yearInput').value;
+    const res = await fetch('/search/year', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ year })
+    });
+    const data = await res.json();
+    if (data.error || data.length === 0) return showError('No movies found for that year');
+    renderNormalTable(data);
+  });
+
+});
 
